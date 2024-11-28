387e4e5c333c70d47f56186fd0521a7e2b62d83b
Subbulakshmi2204 <subbu22april2006@gmail.com>
2024-11-28 14:25:42 +0530

    Add files via upload

---
+++ python program -3.py
@@ -0,0 +1,23 @@
+#find the area of rectangle,square,triangle and circle
+
+#area of the rectangle
+l=int(input("Enter the length of the rectangle:"))
+b=int(input("Enter the breadth of the rectangle:"))
+a=l*b
+print("The area of the rectangle is",a)
+
+#area of the square
+s=int(input("Enter the side of the square:"))
+a=s*s
+print("The area of the square is",a)
+
+#area of the triangle
+b=int(input("Enter the base of the triangle:"))
+h=int(input("Enter the height of the triangle:"))
+a=1/2*b*h
+print("The area of the triangle is",a)
+
+#area of circle
+r=int(input("Enter the radius of circle:"))
+a=3.14*r*r
+print("The area of the circle is",a)
